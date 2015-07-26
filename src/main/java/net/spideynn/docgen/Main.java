1b9c4d93933b04e6895ad703f4b4894cbad6aba5
Spideynn <spideynn@gmail.com>
2015-07-26 12:56:25 -0400

    Intial code commit.
    Signed-off-by: Spideynn <spideynn@gmail.com>

---
+++ src/main/java/net/spideynn/docgen/Main.java
@@ -0,0 +1,21 @@
+package net.spideynn.docgen;
+
+public class Main {
+
+	private String	defaultInput	= "";
+	private String	defaultOutput	= "";
+
+	public static void main(String[] args) {
+		/*
+		 * switch (args[0]) {
+		 * case "--in":
+		 * break;
+		 * case "--out":
+		 * break;
+		 * default:
+		 * break;
+		 * }
+		 */
+	}
+
+}
