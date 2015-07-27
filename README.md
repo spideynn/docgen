280ab3b4e7826f30fd9459a416e0ee6674b1fb27
Spideynn <spideynn@gmail.com>
2015-07-26 20:19:38 -0400

    Made some algorithim changes, and readme changes. WIP
    Signed-off-by: Spideynn <spideynn@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docgen
 Generate documentation from a text document.
+
+# Usage
+**The text document has to have the first line being the title, second line is the copyright, and rest is text.**
+`` java -jar docgen.jar --in <filename.txt> --out <filename.html> ``
+
+# Using your own template (WIP support)
+
+**Your template MUST have an area for a title, text and copyright.**
+
+ex. ``{{copyright}}``, ``{{title}}``, ``{{text}}``
+
+Specify your template via ``--template <filename.html>``
--- src/main/java/net/spideynn/docgen/Main.java
+++ src/main/java/net/spideynn/docgen/Main.java
@@ -1,21 +1,102 @@
-package net.spideynn.docgen;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	private String	defaultInput	= "";
-	private String	defaultOutput	= "";
-
 	public static void main(String[] args) {
-		/*
-		 * switch (args[0]) {
-		 * case "--in":
-		 * break;
-		 * case "--out":
-		 * break;
-		 * default:
-		 * break;
-		 * }
-		 */
-	}
 
+		try {
+
+			BufferedReader txtfile = new BufferedReader(new FileReader(""));
+			OutputStream htmlfile = new FileOutputStream(new File(""));
+			PrintStream printhtml = new PrintStream(htmlfile);
+
+			String[] txtbyLine = new String[500];
+			String temp = "";
+			String txtfiledata = "";
+
+			String htmlheader = "<html><head>";
+			htmlheader += "<title>Equivalent HTML</title>";
+			htmlheader += "</head><body>";
+			String htmlfooter = "</body></html>";
+			int linenum = 0;
+
+			while ((txtfiledata = txtfile.readLine()) != null) {
+				txtbyLine[linenum] = txtfiledata;
+				linenum++;
+			}
+			for (int i = 0; i < linenum; i++) {
+				if (i == 0) {
+					temp = htmlheader + txtbyLine[0];
+					txtbyLine[0] = temp;
+				}
+				if (linenum == i + 1) {
+					temp = txtbyLine[i] + htmlfooter;
+					txtbyLine[i] = temp;
+				}
+				printhtml.println(txtbyLine[i]);
+			}
+
+			printhtml.close();
+			htmlfile.close();
+			txtfile.close();
+
+		}
+
+		catch (Exception e) {
+		}
+	}
+	
+	private static String txtToHtml(String s) {
+        StringBuilder builder = new StringBuilder();
+        boolean previousWasASpace = false;
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                if (previousWasASpace) {
+                    builder.append("&nbsp;");
+                    previousWasASpace = false;
+                    continue;
+                }
+                previousWasASpace = true;
+            } else {
+                previousWasASpace = false;
+            }
+            switch (c) {
+                case '<':
+                    builder.append("&lt;");
+                    break;
+                case '>':
+                    builder.append("&gt;");
+                    break;
+                case '&':
+                    builder.append("&amp;");
+                    break;
+                case '"':
+                    builder.append("&quot;");
+                    break;
+                case '\n':
+                    builder.append("<br>");
+                    break;
+                // We need Tab support here, because we print StackTraces as HTML
+                case '\t':
+                    builder.append("&nbsp; &nbsp; &nbsp;");
+                    break;
+                default:
+                    builder.append(c);
+
+            }
+        }
+        String converted = builder.toString();
+        String str = "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\'\".,<>?«»“”‘’]))";
+        Pattern patt = Pattern.compile(str);
+        Matcher matcher = patt.matcher(converted);
+        converted = matcher.replaceAll("<a href=\"$1\">$1</a>");
+        return converted;
+    }
 }
